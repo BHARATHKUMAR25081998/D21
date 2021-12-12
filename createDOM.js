60553d53f111bb0ca829c619f0cf8f8e9cc5d760
BHARATHKUMAR25081998 <87696403+BHARATHKUMAR25081998@users.noreply.github.com>
2021-12-12 16:57:22 +0530

    Add files via upload

---
+++ createDOM.js
@@ -0,0 +1,14 @@
+var baseDiv=document.createElement('div');
+
+var s=document.createElement('span');
+s.id='counter';
+s.innerText='Get Ready !';
+
+div1=document.createElement('div');
+div1.id="message";
+
+baseDiv.appendChild(s);
+baseDiv.append(div1);
+
+document.body.append(baseDiv);
+
